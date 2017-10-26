477d531b449c6664af3b4c1415114a9dea3a7f25
JasonLee's iMac <1021811501@qq.com>
2017-10-26 10:23:37 +0800

    basetextview

---
+++ MaiDianPu/Base/DGTextView.h
@@ -0,0 +1,13 @@
+//
+//  DGTextView.h
+//  warenqi
+//
+//  Created by JasonLee on 2017/7/24.
+//  Copyright © 2017年 Mr.Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DGTextView : UITextView
+@property(nullable, nonatomic,copy) IBInspectable NSString    *placeholder;
+@end
